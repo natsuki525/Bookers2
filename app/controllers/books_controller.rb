f1d9efc0bdac87446ad7cdf70abc047c0b0bf1e4
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-15 02:05:03 +0900

    [Add] 投稿機能・投稿一覧画面追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+	# 投稿画面を表示
+	def new
+		@book = Book.new
+	end
+	# 投稿を保存
+	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to books_path
+	end
+	# 投稿のリストを表示する画面を作る
+	def index
+		@books = Book.all
+	end
+	# 投稿の詳細を表示する画面を作る
+	def show
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+end
