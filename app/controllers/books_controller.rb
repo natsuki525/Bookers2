5d9e0805be9df5d58ec5ceb08f113d127f00264c
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-16 13:51:23 +0900

    [Add] 投稿削除・編集機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		@book.save
-		redirect_to books_path
+		redirect_to @book
 	end
 	# 投稿のリストを表示する画面を作る
 	def index
@@ -23,6 +23,16 @@ class BooksController < ApplicationController
 		@book =Book.find(params[:id])
 		@book.destroy
 		redirect_to books_path
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
+		redirect_to book_path(book.id)
 	end
 
 	private
